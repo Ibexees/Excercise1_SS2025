c74dac6e155af91d45222f2588f6dd2d825ece0f
Marius <marius.marik.19@gmail.com>
2025-03-04 22:55:17 +0100

    Docs: title_filter_not_case_sensitive() added comments

--- src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -108,35 +108,10 @@ class HomeControllerTest {
 
         //erwartete Anzahl an Treffern
         assertEquals(2, resultDarkKnight.size());
-
-        /*assertTrue(resultDarkKnight.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("The Dark Knight")));
-        assertFalse(resultDarkKnight.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("Superbad")));
-
-        //suche superbad
-
-        List<Movie> resultSuperbad = homeController.filterMovies(null,"Superbad");
-
-        assertEquals(2,resultSuperbad.size());
-        assertTrue(resultSuperbad.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("Superbad")));
-        assertTrue(resultSuperbad.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("SUPERBAD")));
-
-        assertTrue(resultDarkKnight.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("The Dark Knight")));
-        assertTrue(resultDarkKnight.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("the dark knight")));
-
-        //suche willy wonka
-        String searchQuery2="willy wonka and the chocolate factory";
-        List<Movie> resultWillyWonka = homeController.filterMovies(null,"willy wonka and the chocolate factory");
-
-        assertEquals(2,resultWillyWonka.size());
-        assertTrue(resultWillyWonka.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("Willy Wonka And The Chocolate Factory")));
-        assertTrue(resultWillyWonka.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("willy wonka and the chocolate factory")));
+        assertTrue(resultDarkKnight.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("The Dark Knight"))); //braucht man diese Zeile Robuster aber Logik ?
+        assertFalse(resultDarkKnight.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("Superbad"))); //braucht man diese Zeile Robuster aber Logik ?
 
 
-        List<Movie> resultScarface = homeController.filterMovies(null,"scarface");
-
-        assertEquals(2,resultScarface.size());
-        assertTrue(resultScarface.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("SCARFACE")));
-        assertTrue(resultScarface.stream().anyMatch(movie -> movie.getTitle().equalsIgnoreCase("scarface")));*/
     }
 
     @Test //Iyobosa
