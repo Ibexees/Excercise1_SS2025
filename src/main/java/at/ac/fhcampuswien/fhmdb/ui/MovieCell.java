cd6d3b7cc09a044fc2a37c0bfe124a1d86329b18
Marius <marius.marik.19@gmail.com>
2025-03-21 17:38:47 +0100

    refactor: MovieCell now displays new attribotes from API

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -9,11 +9,20 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
+import java.util.Arrays;
+
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genres = new Label();
-    private final VBox layout = new VBox(title, detail, genres);
+    private final Label releaseYear = new Label();
+    private String imgUrl;
+    private final Label lengthInMinutes = new Label();
+    private final Label mainCast = new Label();
+    private final Label directors = new Label();
+    private final Label writers = new Label();
+    private final Label rating = new Label();
+    private final VBox layout = new VBox(title, detail, genres,releaseYear,rating,lengthInMinutes,mainCast,directors,writers);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -31,12 +40,26 @@ public class MovieCell extends ListCell<Movie> {
                             : "No description available"
             );
             genres.setText(movie.getGenresAsString());
+            releaseYear.setText("Release Year: " + Integer.toString(movie.getReleaseYear()));
+           lengthInMinutes.setText("length in minutes: "+Integer.toString(movie.getLengthInMinutes()));
+           rating.setText("Rating out of 10: " +Double.toString( movie.getRating()));
 
+
+
+            mainCast.setText("Main Cast: " +Arrays.toString(movie.getMainCast()));
+            writers.setText("writers: " + Arrays.toString(movie.getWriters()));
+            directors.setText("directors: " + Arrays.toString(movie.getDirectors()));
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
             genres.getStyleClass().add("text-white");
+            releaseYear.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
+            mainCast.getStyleClass().add("text-white");
+            writers.getStyleClass().add("text-white");
+            directors.getStyleClass().add("text-white");
+            lengthInMinutes.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
